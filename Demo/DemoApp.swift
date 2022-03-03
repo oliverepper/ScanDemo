79c8ae0eae5b54a7daa4cd3ef6522a384c045549
Oliver Epper <oliver.epper@gmail.com>
2022-03-03 08:09:33 +0100

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Oliver Epper on 03.03.22.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
